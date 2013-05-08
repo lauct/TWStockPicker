48b7657187ff039ea741b2d33470e4a953997e59
Justin Liu <mangoliu@gmail.com>
2013-05-09 01:32:12 +0800

    save twee and gtsm id to lists

--- python_test_sample/readcsv.py
+++ python_test_sample/readcsv.py
@@ -4,18 +4,22 @@ __author__ = 'chun'
 
 import csv
 
-stock_id_list = []
+twse_id_list = []
+gtsm_id_list = []
 csv_file_list = ['TWSE_price_ranking.csv', 'GTSM_price_ranking.csv']
 
 for filename in csv_file_list:
     with open(filename, 'rb') as csvfile:
         twsereader = csv.DictReader(csvfile, ['代碼', '名稱', '成交'])
-        print "----------------------------------"
         for row in twsereader:
             if row['代碼'] == '代碼':
                 pass
             else:
-                print row['代碼']
-        print "----------------------------------"
-
+                if filename == csv_file_list[0]:
+                    twse_id_list.append(row['代碼'])
+                if filename == csv_file_list[1]:
+                    gtsm_id_list.append(row['代碼'])
     csvfile.close()
+
+print twse_id_list
+print gtsm_id_list
