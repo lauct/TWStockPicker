fea9ed69bd64c209394062f963f680e857e9006c
Justin Liu <mangoliu@gmail.com>
2013-05-09 01:51:40 +0800

    make readcsv to a module

--- python_test_sample/readcsv.py
+++ python_test_sample/readcsv.py
@@ -4,22 +4,30 @@ __author__ = 'chun'
 
 import csv
 
-twse_id_list = []
-gtsm_id_list = []
-csv_file_list = ['TWSE_price_ranking.csv', 'GTSM_price_ranking.csv']
 
-for filename in csv_file_list:
-    with open(filename, 'rb') as csvfile:
-        twsereader = csv.DictReader(csvfile, ['代碼', '名稱', '成交'])
-        for row in twsereader:
-            if row['代碼'] == '代碼':
-                pass
-            else:
-                if filename == csv_file_list[0]:
-                    twse_id_list.append(row['代碼'])
-                if filename == csv_file_list[1]:
-                    gtsm_id_list.append(row['代碼'])
-    csvfile.close()
+def readcsv():
+    twse_id_list = []
+    gtsm_id_list = []
+    stock_id_dict = {}
+    csv_file_list = ['TWSE_price_ranking.csv', 'GTSM_price_ranking.csv']
 
-print twse_id_list
-print gtsm_id_list
+    for filename in csv_file_list:
+        with open(filename, 'rb') as csvfile:
+            twsereader = csv.DictReader(csvfile, ['代碼', '名稱', '成交'])
+            for row in twsereader:
+                if row['代碼'] == '代碼':
+                    pass
+                else:
+                    if filename == csv_file_list[0]:
+                        twse_id_list.append(row['代碼'])
+                    if filename == csv_file_list[1]:
+                        gtsm_id_list.append(row['代碼'])
+        csvfile.close()
+
+    stock_id_dict['TWSE_ID_LIST'] = twse_id_list
+    stock_id_dict['GTSM_ID_LIST'] = gtsm_id_list
+    print stock_id_dict
+    return stock_id_dict
+
+# debug log
+# readcsv()
